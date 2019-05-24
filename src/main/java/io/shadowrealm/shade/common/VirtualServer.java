ef9b8c636d7a0ec11976af663647db1b77c3fdb8
cyberpwn <danielmillst@gmail.com>
2019-05-24 01:25:05 -0400

    Virtual server support extra data

--- src/main/java/io/shadowrealm/shade/common/VirtualServer.java
+++ src/main/java/io/shadowrealm/shade/common/VirtualServer.java
@@ -1,15 +1,25 @@
 package io.shadowrealm.shade.common;
 
+import mortar.compute.math.M;
+
 public class VirtualServer
 {
 	private String id;
 	private String name;
 	private String route;
+	private String status;
+	private String tagline;
+	private long since;
+	private int count;
 	private int port;
 	private RestlessConnector connector;
 
 	public VirtualServer(String id, String name, String route, int port)
 	{
+		status = "&eSyncronizing...";
+		tagline = "";
+		since = M.ms();
+		count = 0;
 		this.id = id;
 		this.name = name;
 		this.port = port;
@@ -45,5 +55,75 @@ public class VirtualServer
 	public int getPort()
 	{
 		return port;
+	}
+
+	public String getStatus()
+	{
+		return status;
+	}
+
+	public void setStatus(String status)
+	{
+		this.status = status;
+	}
+
+	public String getTagline()
+	{
+		return tagline;
+	}
+
+	public void setTagline(String tagline)
+	{
+		this.tagline = tagline;
+	}
+
+	public long getSince()
+	{
+		return since;
+	}
+
+	public void setSince(long since)
+	{
+		this.since = since;
+	}
+
+	public int getCount()
+	{
+		return count;
+	}
+
+	public void setCount(int count)
+	{
+		this.count = count;
+	}
+
+	public RestlessConnector getConnector()
+	{
+		return connector;
+	}
+
+	public void setConnector(RestlessConnector connector)
+	{
+		this.connector = connector;
+	}
+
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public void setRoute(String route)
+	{
+		this.route = route;
+	}
+
+	public void setPort(int port)
+	{
+		this.port = port;
 	}
 }
