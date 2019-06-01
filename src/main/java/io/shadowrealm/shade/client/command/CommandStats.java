f58c91b269c8c9da7a1116bf6384c6d387572192
cyberpwn <danielmillst@gmail.com>
2019-06-01 06:06:30 -0400

    Stats command support offline or off-server and only allow admins

--- src/main/java/io/shadowrealm/shade/client/command/CommandStats.java
+++ src/main/java/io/shadowrealm/shade/client/command/CommandStats.java
@@ -1,13 +1,18 @@
 package io.shadowrealm.shade.client.command;
 
+import java.util.UUID;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.inventory.meta.BookMeta.Generation;
 
 import io.shadowrealm.shade.client.Shade;
+import io.shadowrealm.shade.client.ShadeClient;
 import io.shadowrealm.shade.common.Statistics;
+import mortar.api.world.P;
 import mortar.bukkit.command.MortarCommand;
 import mortar.bukkit.command.MortarSender;
 import mortar.lang.collection.GList;
@@ -18,15 +23,42 @@ public class CommandStats extends MortarCommand
 	public CommandStats()
 	{
 		super("stats");
+		requiresPermission(ShadeClient.perm.stats);
 	}
 
 	@Override
 	public boolean handle(MortarSender sender, String[] args)
 	{
-		Statistics s = Shade.getStatistics(sender.player());
+		if(args.length == 0)
+		{
+			sender.sendMessage("/stats <player>");
+			return true;
+		}
+
+		String name = args[0];
+		UUID id = null;
+
+		for(Player i : P.onlinePlayers())
+		{
+			if(i.getName().equals(name))
+			{
+				id = i.getUniqueId();
+				break;
+			}
+		}
+
+		id = id == null ? Shade.getUUID(name) : id;
+
+		if(id == null)
+		{
+			sender.sendMessage("Cant find " + name);
+			return true;
+		}
+
+		Statistics s = Shade.getStatistics(id);
 		ItemStack is = new ItemStack(Material.WRITTEN_BOOK);
 		BookMeta bm = (BookMeta) is.getItemMeta();
-		bm.setTitle(sender.player().getName() + "'s Statistics");
+		bm.setTitle(name + "'s Statistics");
 		GList<String> pages = new GList<String>();
 		bm.setAuthor("ShadowRealm");
 		int m = 0;
