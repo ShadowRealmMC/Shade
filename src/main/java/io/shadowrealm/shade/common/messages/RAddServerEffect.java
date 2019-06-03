ddc9bb2265d55b9f3a9bbbf29194588cfd8bbb15
cyberpwn <danielmillst@gmail.com>
2019-06-03 14:05:33 -0400

    Add effect message

---
+++ src/main/java/io/shadowrealm/shade/common/messages/RAddServerEffect.java
@@ -0,0 +1,54 @@
+package io.shadowrealm.shade.common.messages;
+
+import io.shadowrealm.shade.client.Shade;
+import io.shadowrealm.shade.common.RestlessObject;
+import io.shadowrealm.shade.common.ServerEffect;
+import io.shadowrealm.shade.common.ServerEffects;
+import io.shadowrealm.shade.server.ShadeServer;
+
+public class RAddServerEffect extends RestlessObject
+{
+	private ServerEffects effects;
+	private ServerEffect effect;
+	private String player;
+
+	@Override
+	public RestlessObject handle()
+	{
+		ShadeServer.instance.updateServerEffects(effects(), effect(), player(), Shade.getUnlock(effect.getId()).getFormattedName());
+		return new ROK();
+	}
+
+	public ServerEffects effects()
+	{
+		return effects;
+	}
+
+	public RAddServerEffect effects(ServerEffects effects)
+	{
+		this.effects = effects;
+		return this;
+	}
+
+	public ServerEffect effect()
+	{
+		return effect;
+	}
+
+	public RAddServerEffect effect(ServerEffect effect)
+	{
+		this.effect = effect;
+		return this;
+	}
+
+	public String player()
+	{
+		return player;
+	}
+
+	public RAddServerEffect player(String player)
+	{
+		this.player = player;
+		return this;
+	}
+}
