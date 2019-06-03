c77eb374e1cbab3b0d9b68b6f6c57fe92363a5c3
cyberpwn <danielmillst@gmail.com>
2019-06-03 13:40:04 -0400

    Booster module

--- src/main/java/io/shadowrealm/shade/module/SMBoosters.java
+++ src/main/java/io/shadowrealm/shade/module/SMBoosters.java
@@ -1,17 +1,13 @@
 package io.shadowrealm.shade.module;
 
-import org.bukkit.entity.Player;
-
+import io.shadowrealm.shade.client.command.CommandBooster;
 import io.shadowrealm.shade.client.permission.PermissionShade;
 import io.shadowrealm.shade.module.api.ShadeModule;
 import mortar.api.config.Key;
 import mortar.bukkit.command.Permission;
-import mortar.lang.collection.GMap;
 
 public class SMBoosters extends ShadeModule
 {
-	private GMap<Player, Long> lastDurabilityWarning;
-
 	@Key("enable")
 	public static boolean enabled = true;
 
@@ -26,7 +22,7 @@ public class SMBoosters extends ShadeModule
 	@Override
 	public void start()
 	{
-
+		registerCommand(new CommandBooster());
 	}
 
 	@Override
